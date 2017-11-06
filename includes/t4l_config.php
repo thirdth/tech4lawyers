c77ff761a6ab46bbc1d9ba038809dd5c3532b627
Zack Glaser <zackglaser@gmail.com>
2017-11-06 15:57:39 -0600

    reverted db variables to remote db

--- includes/t4l_config.php
+++ includes/t4l_config.php
@@ -6,12 +6,12 @@ function get_connected()  {
   $dbaddyLive = "mysql.tech4lawyers.com";
   $dbaddy = "Localhost";
   $dbuserLive = "tech4lawyerscom";
-  $dbuser = "zack";
-  $dbpwLive = "JWLnZT4b";
+  $dbuser = "Zack";
+  $dbpwLive = "JWLnZT4b"
   $dbpw = "glockner605";
   $dbdbLive = "tech4lawyers_com";
   $dbdb = "tech4lawyers";
-  $conn = mysqli_connect('mysql.tech4lawyers.com', 'tech4lawyerscom', 'JWLnZT4b', 'tech4lawyers_com');
+  $conn = mysqli_connect("mysql.tech4lawyers.com", "tech4lawyerscom", "JwLnZT4b", "tech4lawyers_com");
   if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
   }
@@ -21,18 +21,21 @@ function get_connected()  {
 function shorten_string($string, $wordsreturned)
 /*  Returns the first $wordsreturned out of $string.  If string
 contains fewer words than $wordsreturned, the entire string
-is returned. */
+is returned.
+*/
 {
 $retval = $string;      //  Just in case of a problem
 
 $array = explode(" ", $string);
 if (count($array)<=$wordsreturned)
-//  Already short enough, return the whole thing
+/*  Already short enough, return the whole thing
+*/
 {
 $retval = $string;
 }
 else
-//  Need to chop of some words
+/*  Need to chop of some words
+*/
 {
 array_splice($array, $wordsreturned);
 $retval = implode(" ", $array)." . . .";
