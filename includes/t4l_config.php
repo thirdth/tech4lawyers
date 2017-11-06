3be80e0a10d952584e8ed6fd11155f228fb6b1ab
Zack Glaser <zackglaser@gmail.com>
2017-11-06 13:08:06 -0600

    cleaned up comments in t4l_config file

--- includes/t4l_config.php
+++ includes/t4l_config.php
@@ -21,21 +21,18 @@ function get_connected()  {
 function shorten_string($string, $wordsreturned)
 /*  Returns the first $wordsreturned out of $string.  If string
 contains fewer words than $wordsreturned, the entire string
-is returned.
-*/
+is returned. */
 {
 $retval = $string;      //  Just in case of a problem
 
 $array = explode(" ", $string);
 if (count($array)<=$wordsreturned)
-/*  Already short enough, return the whole thing
-*/
+//  Already short enough, return the whole thing
 {
 $retval = $string;
 }
 else
-/*  Need to chop of some words
-*/
+//  Need to chop of some words
 {
 array_splice($array, $wordsreturned);
 $retval = implode(" ", $array)." . . .";
