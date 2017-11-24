0aed20c2ed817dd91d720d4e8baba894ce73e63a
Zack Glaser <zackglaser@gmail.com>
2017-11-24 12:24:08 -0600

    added date to articles

--- articles.php
+++ articles.php
@@ -21,6 +21,7 @@ foreach ($results as $result){
   // Decode the shortened string so it can display HTML
   $body = html_entity_decode($blip);
   echo "        <div class='col-md-12 article'>
+                  <small>" . $result['Date_created'] . "</small>
                   <h2>" . $result['Title'] . "</h2>
                   <h3>" . $result['Stitle'] . "</h3>
                   <p>" . $body . "</strong></p>
