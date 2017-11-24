948530cd8b3aeab4a85322feed7bf2aa03b77edc
Zack Glaser <zackglaser@gmail.com>
2017-11-24 12:41:25 -0600

    add order by to articles page

--- articles.php
+++ articles.php
@@ -4,7 +4,7 @@ include 'includes/header.php';
 $conn = get_connected();
 // CONTROLER //
 // get all info from articles table
-$sql = "SELECT * from articles where Live=1";
+$sql = "SELECT * from articles where Live=1 ORDER By Date_created";
 $results = mysqli_query($conn, $sql);
 // close the connection to the database
 mysqli_close($conn);
